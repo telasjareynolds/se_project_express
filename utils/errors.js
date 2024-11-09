b4875b0e7f0ef89b62958b08f53083b29eb51bb3
Telasja Reynolds <telasjareynolds@gmail.com>
2024-11-09 18:44:51 -0500

    fix: fix validation status

--- utils/errors.js
+++ utils/errors.js
@@ -4,19 +4,19 @@ function checkErrors(err, res) {
   if (err.name === "ValidationError") {
     res.status(400).send({ message: err.message });
   } else if (err.name === "CastError") {
-    res.status(400).send({ message: "Invalid ID format" });
+    res.status(400).send({ message: err.message });
   } else if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-    res.status(400).send({ message: "Malformed JSON in request body" });
+    res.status(400).send({ message: err.message });
   } else if (err.code === 11000) {
-    res.status(409).send({ message: "Duplicate key error" });
+    res.status(409).send({ message: err.message });
   } else if (err.name === "NotFoundError" || err.name === "DocumentNotFoundError" || err.name === "AssertionError") {
-    res.status(404).send({ message: "Resource not found" });
+    res.status(404).send({ message: err.message });
   } else if (err.name === "UnauthorizedError") {
-    res.status(401).send({ message: "Unauthorized access" });
+    res.status(401).send({ message: err.message });
   } else if (err.name === "ForbiddenError") {
-    res.status(403).send({ message: "Forbidden" });
+    res.status(403).send({ message: err.message });
   } else {
-    res.status(500).send({ message: "An internal server error occurred" });
+    res.status(500).send({ message: err.message });
   }
 }
 module.exports = { checkErrors };
